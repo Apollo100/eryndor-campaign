c36f622ecf2d4a75ea0e7204b125ff408412f0a2
Apollo100 <49254805+Apollo100@users.noreply.github.com>
2025-09-30 01:48:26 -0500

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
+// --- Firebase Config ---
+const firebaseConfig = {
+    apiKey: "AIzaSyA7Ss7d5s2YH2IgGek5WJd0nDL4Fl7r9ro",
+    authDomain: "eryndor-d5d2d.firebaseapp.com",
+    projectId: "eryndor-d5d2d",
+    storageBucket: "eryndor-d5d2d.firebasestorage.app",
+    messagingSenderId: "693835704456",
+    appId: "1:693835704456:web:a11df8adfd343f8761d73d"
+};
+
+// --- Initialize Firebase ---
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+let currentUserId = null;
+let currentUserEmail = null;
+
+export { auth, db, currentUserId, currentUserEmail };
+
 import './firebase-init.js';
 import './nav.js';
 import './pc.js';
